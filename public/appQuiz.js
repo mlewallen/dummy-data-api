5fc974293c07cf534a2e6ed03e8ef71786704b6e
Michael Lewallen <mlewallen1@outlook.com>
2018-08-06 00:20:57 -0400

    Study/Quiz Bugs
    Get study section to work properly with pagination as well as arrow keys. Set up quiz structure and get basic functionality/styling to quiz section.

--- public/appQuiz.js
+++ public/appQuiz.js
@@ -1,37 +1,163 @@
-document.addEventListener('DOMContentLoaded', function() {
+//Global constants
+const questionsContainer = document.getElementById('questions');
+const resultsContainer = document.getElementById('results');
+const submitButton = document.getElementById('submit');
 
-    //Global constants
-    const questions = document.getElementById('questions');
-    const resultsContainer = document.getElementById('results');
-    const submitButton = document.getElementById('submit');
+//JSON Request
+let request = new XMLHttpRequest();
 
-    //JSON Request
-    let request = new XMLHttpRequest();
+request.open('GET', 'http://localhost:3000/data', true);
 
-    request.open('GET', 'http://localhost:3000/data', true);
+request.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
 
-    request.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
+      // Parse out the reponse
+      let data = JSON.parse(this.responseText);
 
-            // Parse out the reponse
-            let data = JSON.parse(this.responseText);
-
-            // Let's add the data we parsed out to the #pickPerson select dynamically
-            data.forEach(function(i){
-                let option = document.createElement('OPTION');
-                option.setAttribute('value', i.id - 1);
-                let name = document.createTextNode(i.name);
-                option.appendChild(name);
-                select.appendChild(option);
-            });
-
-        } else if (this.status == 404){
-            //Error message if request fails
-            let error = document.createElement('h1');
-            let errorMsg = error.innterHTML = 'Oh snap it be broke.';
-            document.getElementById('app').append(error);
+      const questions = [
+        {
+          question: data[0].image_url,
+          answers: {
+            a: data[0].quote,
+            b: data[7].quote,
+            c: data[2].quote
+          },
+          correctAnswer: 'a'
+        },
+        {
+          question: data[1].image_url,
+          answers: {
+            a: data[0].quote,
+            b: data[1].quote,
+            c: data[8].quote
+          },
+          correctAnswer: 'b'
+        },
+        {
+          question: data[2].image_url,
+          answers: {
+            a: data[3].quote,
+            b: data[5].quote,
+            c: data[2].quote
+          },
+          correctAnswer: 'c'
         }
-    }
-    request.send();
+      ];
 
-});
+      function buildQuiz() {
+        // we'll need a place to store the HTML output
+        const output = [];
+
+        // for each question...
+        questions.forEach((currentQuestion, questionNumber) => {
+          // we'll want to store the list of answer choices
+          const answers = [];
+
+          // and for each available answer...
+          for (letter in currentQuestion.answers) {
+            // ...add an HTML radio button
+            answers.push(
+              `<div class="answer">
+                <label>
+                   <input type="radio" name="question${questionNumber}" value="${letter}">${letter} : ${currentQuestion.answers[letter]}
+                 </label>
+               </div>`
+            );
+          }
+
+          // add this question and its answers to the output
+          output.push(
+            `<div class="slide">
+               <div class="scientist-image image-wrapper" id="image" style="background-image: url(${currentQuestion.question}"></div>
+               <div class="answers">${answers.join("")}</div>
+             </div>`
+          );
+        });
+
+        // finally combine our output list into one string of HTML and put it on the page
+        questionsContainer.innerHTML = output.join("");
+      }
+
+      function showResults() {
+        // gather answer containers from our quiz
+        const answerContainers = questionsContainer.querySelectorAll(".answers");
+
+        // keep track of user's answers
+        let numCorrect = 0;
+
+        // for each question...
+        questions.forEach((currentQuestion, questionNumber) => {
+          // find selected answer
+          const answerContainer = answerContainers[questionNumber];
+          const selector = `input[name=question${questionNumber}]:checked`;
+          const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+
+          // if answer is correct
+          if (userAnswer === currentQuestion.correctAnswer) {
+            // add to the number of correct answers
+            numCorrect++;
+
+            // color the answers green
+            answerContainers[questionNumber].style.color = "lightgreen";
+          } else {
+            // if answer is wrong or blank
+            // color the answers red
+            answerContainers[questionNumber].style.color = "red";
+          }
+        });
+
+        // show number of correct answers out of total
+        resultsContainer.innerHTML = `${numCorrect} out of ${questions.length}`;
+      }
+
+      function showSlide(n) {
+        slides[currentSlide].classList.remove("active-slide");
+        slides[n].classList.add("active-slide");
+        currentSlide = n;
+
+        if (currentSlide === 0) {
+          previousButton.style.display = "none";
+        } else {
+          previousButton.style.display = "inline-block";
+        }
+
+        if (currentSlide === slides.length - 1) {
+          nextButton.style.display = "none";
+          submitButton.style.display = "inline-block";
+        } else {
+          nextButton.style.display = "inline-block";
+          submitButton.style.display = "none";
+        }
+      }
+
+      function showNextSlide() {
+        showSlide(currentSlide + 1);
+      }
+
+      function showPreviousSlide() {
+        showSlide(currentSlide - 1);
+      }
+
+      // display quiz right away
+      buildQuiz();
+
+      const previousButton = document.getElementById("previous");
+      const nextButton = document.getElementById("next");
+      const slides = document.querySelectorAll(".slide");
+      let currentSlide = 0;
+
+      showSlide(0);
+
+      // on submit, show results
+      submitButton.addEventListener("click", showResults);
+      previousButton.addEventListener("click", showPreviousSlide);
+      nextButton.addEventListener("click", showNextSlide);
+
+    } else if (this.status == 404){
+        //Error message if request fails
+        let error = document.createElement('h1');
+        let errorMsg = error.innterHTML = 'Oh snap it be broke.';
+        document.getElementById('app').append(error);
+    }
+}
+request.send();
--- public/appStudy.js
+++ public/appStudy.js
@@ -1,96 +1,93 @@
-document.addEventListener('DOMContentLoaded', function() {
+//Global constants
+const select = document.getElementById('pickPerson');
 
-    //Global constants
-    const select = document.getElementById('pickPerson');
+//JSON Request
+let request = new XMLHttpRequest();
+request.open('GET', 'http://localhost:3000/data', true);
+request.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
 
-    //JSON Request
-    let request = new XMLHttpRequest();
+        // Parse out the reponse
+        let data = JSON.parse(this.responseText);
 
-    request.open('GET', 'http://localhost:3000/data', true);
+        // Let's add the data we parsed out to the #pickPerson select dynamically
+        data.forEach(function(i){
+            let option = document.createElement('OPTION');
+            option.setAttribute('value', i.id);
+            let name = document.createTextNode(i.name);
+            option.appendChild(name);
+            select.appendChild(option);
+        });
 
-    request.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-
-            // Parse out the reponse
-            let data = JSON.parse(this.responseText);
-
-            // Let's add the data we parsed out to the #pickPerson select dynamically
-            data.forEach(function(i){
-                let option = document.createElement('OPTION');
-                option.setAttribute('value', i.id - 1);
-                let name = document.createTextNode(i.name);
-                option.appendChild(name);
-                select.appendChild(option);
-            });
-
-            // When the select changes value let's update the app
-            function selectChange() {
-                let selectedOption;
-                let len = select.length;
-                for (let i = 0; i < len; i++) {
-                    selectedOption = select.options[i];
-                    if(selectedOption.selected === true) {
-                        let id = select.options[i].value;
-                        updateApp(id);
-                    }
-                }
-                return selectedOption;
-            }
-            document.addEventListener('DOMContentLoaded', selectChange());
-            select.addEventListener('change', selectChange);
-
-            // Button next code to cycle through options
-            function selectChangeNext() {                
-                let val = parseInt(select.options[select.selectedIndex].value);
-                let id;
-                if(val === select.length - 1) {
-                    id =  0;
-                    console.log('id end=' + val + 'len=' + select.length);
-                    return id;
-                    
-                } else {
-                    id = val + 1;
-                    console.log('id=' + id);
-                    return id;                    
+        // When the select changes value let's update the app
+        function selectChange() {
+            let selectedOption;
+            let len = select.length;
+            for (let i = 0; i < len; i++) {
+                selectedOption = select.options[i];
+                if(selectedOption.selected === true) {
+                    updateApp(i);
                 }
             }
-            document.getElementById('next').addEventListener('click', function () {
-                select.options.selectedIndex = selectChangeNext();
-                updateApp(selectChangeNext() - 1);
-            });
-
-            // Button previous code to cycle through options in reverse
-            function selectChangePrev() {
-                let val = parseInt(select.options[select.selectedIndex].value);
-                let id;
-                if(val === 0) {
-                    id = select.length - 1;
-                } else {
-                    id = val - 1;
-                }
-                return id;
-            }
-            document.getElementById('prev').addEventListener('click', function () {
-                select.options.selectedIndex = selectChangePrev();
-                updateApp(selectChangePrev() + 1);
-            });
-
-            // Update app with data from JSON
-            function updateApp(id) {
-                //console.log(id);
-                document.getElementById('name').innerHTML = data[id].name;
-                document.getElementById('bday').innerHTML = data[id].birthday;
-                document.getElementById('image').style.backgroundImage = 'url(' + data[id].image_url + ')';
-                document.getElementById('quote').innerHTML = data[id].quote;
-            }
-
-        } else if (this.status == 404){
-            //Error message if request fails
-            let error = document.createElement('h1');
-            let errorMsg = error.innterHTML = 'Oh snap it be broke.';
-            document.getElementById('app').append(error);
+            return selectedOption;
         }
-    }
-    request.send();
+        document.addEventListener('DOMContentLoaded', selectChange());
+        select.addEventListener('change', selectChange);
 
-});
+        // Button next code to cycle through options
+        function selectChangeNext(val) {
+            if(val === select.length) {
+                return 0;
+            } else {
+                return val++;
+            }
+        }
+        document.getElementById('next').addEventListener('click', function () {
+            let val = parseInt(select.options[select.selectedIndex].value);
+            select.options.selectedIndex = selectChangeNext(val);
+            updateApp(selectChangeNext(val));
+
+        });
+
+        // Button previous code to cycle through options in reverse
+        function selectChangePrev(val) {
+            if(val === 1) {
+                return select.length - 1;
+            } else {
+                return val - 2;
+            }
+        }
+        document.getElementById('prev').addEventListener('click', function () {
+            let val = parseInt(select.options[select.selectedIndex].value);
+            select.options.selectedIndex = selectChangePrev(val);
+            updateApp(selectChangePrev(val));
+        });
+
+        //add arrow key functionality
+        document.onkeydown = function(e) {
+          let val = parseInt(select.options[select.selectedIndex].value);
+          if (e.keyCode === 39) {
+            select.options.selectedIndex = selectChangeNext(val);
+            updateApp(selectChangeNext(val));
+          } else if (e.keyCode === 37) {
+              select.options.selectedIndex = selectChangePrev(val);
+              updateApp(selectChangePrev(val));
+            }
+        }
+
+        // Update app with data from JSON
+        function updateApp(id) {
+            document.getElementById('name').innerHTML = data[id].name;
+            document.getElementById('bday').innerHTML = data[id].birthday;
+            document.getElementById('image').style.backgroundImage = 'url(' + data[id].image_url + ')';
+            document.getElementById('quote').innerHTML = data[id].quote;
+        }
+
+    } else if (this.status == 404){
+        //Error message if request fails
+        let error = document.createElement('h1');
+        let errorMsg = error.innterHTML = 'Oh snap it be broke.';
+        document.getElementById('app').append(error);
+    }
+}
+request.send();
